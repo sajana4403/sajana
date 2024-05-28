af09f8653cc46a19703999cf3e03db315730180c
sajana4403 <163549566+sajana4403@users.noreply.github.com>
2024-05-27 22:15:03 -0400

    Add files via upload

---
+++ autocomplete.js
@@ -0,0 +1,38 @@
+letavailablekeywords=[];
+'HTML';
+'CSS';
+'Easy Tutorials';
+'Web design tutorials'
+'Javascript'
+'where to learn coding online'
+'Where to learn web design'
+'How to create a website'
+]
+const resultBox= document.querySelector(".result-box");
+const inputBox=document.getElementById("input-box")
+inputBox.onekeyup="function"(){
+    letresult=[]
+    letinput=inputBox.Value;
+
+}
+if(inputBox.length){
+    result=available keywords.filter-((keyword))=>{
+        returnkeyword.toLowercase().includes(input.toLowercase());
+    }
+    console.log(result);
+
+    }
+    display(result)
+    if(!result length){
+        function on display (result)
+        constcontent=result.map((list)=>{return"<li>"on click=selectinput(this)+
+        list+"</li>"
+        resultBox.innerHTML="<ul>"+contentjoin()"</ul>"
+        functionselectinput(list){
+            inputBox.value=list.innerHTML;
+            resultBox.innerHTML="<ul>+content.join(")+"</ul">
+        }
+
+
+    }
+}
